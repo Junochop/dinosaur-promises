2110b1fc358b81edc261fc4a93c30f030f5900ff
Juno Lee <lejuno80@gmail.com>
2017-10-14 11:11:30 -0500

    sassy2

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -8,9 +8,25 @@ function printToDom(strang){
 
 var domString = function(dinosaur){
 	var newMsg = "";
-			newMsg += `<div>`;
-			newMsg += `<h5>${dinosaur.type}<br></h5>`;
-	    	newMsg += `<br>`;	
+			newMsg += `<div class=${dinosaur.info ==='Carnivore' ? 'card-bad' : 'card-good'}>`;
+			newMsg += `<h4>${dinosaur.type}<br></h4>`;
+			newMsg += `<h5>${dinosaur.bio}<br></h5>`;
+			if (dinosaur.info === 'Carnivore') {
+				newMsg += `<h4>Has some tasty snacks.</h4>`;
+			} else {
+				newMsg += `<h4>Has some mean poop snacks.</h4>`;
+			}
+	    	newMsg += `<div class='card-holder'>`;
+	    	dinosaur.snacks.forEach((cat)=> {
+	    		newMsg += `<div class='card'>`;
+	    		newMsg += `<h5>${cat.name}</h5>`;
+	    		newMsg += `<div class='card-image'>`;
+	    		newMsg += `<img src='${cat.imageUrl}'>`;
+	    		newMsg += `</div>`;
+	    		newMsg += `<p class='card-description'>${cat.specialSkill}</p>`;
+	    		newMsg += `</div>`;
+	    	});
+	    	newMsg += `</div>`;	
 			newMsg += `</div>`;
    printToDom(newMsg);
  // $("#dinoMan").append(newMsg);
@@ -21,3 +37,7 @@ var domString = function(dinosaur){
 
 
 module.exports = domString;
+
+
+
+//if carnovore ? apply car-bad : if not apply card-good
